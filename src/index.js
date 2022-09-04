1af965f6df606f48f0182ade4d968320f8971fe1
Maxim_Belyaev <Belmaks2016@mail.ru>
2022-09-04 16:47:18 +0300

    Create function and elements of class

--- src/index.js
+++ src/index.js
@@ -3,30 +3,38 @@ import * as ReactDOMClient from 'react-dom/client';
 
 
 //Делаем записи в константы, затем эти константы записываеим, как параметры метода
-const inputClick = () => console.log("Clicked")
-const mouseOver = () => console.log("Mouse Over")
 
-//Create new component
-function Header() {
-	return(
-		<header>Шапка сайта</header>
-	)
+
+//Create new class component
+class Header extends React.Component {//extends == all inherit from a specific parent class(все наследуем от определенного класса родителя)
+	render() {//function of class(without function before functionName)
+		return(//React.Component - Adress(obrachenie) to object React and to object Component 
+			<header>Шапка сайта</header>
+		)
+	}
 }
 
 //Now we will use components.
 //To create new component, you can create new function or class
 //Add component Header in component App
 //Use arrow function
-const App = () => {//name of function == name of component. 
+class App extends React.Component {//name of function == name of component. 
 	//In fact, it isn't function. It's component(JSX-element)
-	return (
-	<div>
-	<Header /> 
-	<input placeholder='Some text'//React + JSX 
-	onClick={inputClick} onMouseEnter={mouseOver}//React + JSX + два обработчика событий
-	/>
-	</div>
-	)
+	helpText = 'Help text'//without const or let. Why?
+	render () {
+		return (//use this.
+			<div>
+				<Header />
+				<h1>{this.helpText}</h1>
+				<input placeholder='Some text'//React + JSX 
+				onClick={this.inputClick} onMouseEnter={this.mouseOver}//React + JSX + два обработчика событий
+				/>
+			</div>
+		)
+	}
+
+	inputClick = () => console.log("Clicked")
+	mouseOver = () => console.log("Mouse Over")
 }
 
 const app = ReactDOMClient.createRoot(document.getElementById("app"));//object, we will place all information
