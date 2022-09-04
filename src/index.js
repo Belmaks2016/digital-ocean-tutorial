42556abdde96027039713d49442277d64968672a
Maxim_Belyaev <Belmaks2016@mail.ru>
2022-09-04 12:51:54 +0300

    Create component

--- src/index.js
+++ src/index.js
@@ -6,28 +6,14 @@ import * as ReactDOMClient from 'react-dom/client';
 const inputClick = () => console.log("Clicked")
 const mouseOver = () => console.log("Mouse Over")
 
-const elements = <input placeholder='Some text'//React + JSX 
-onClick={inputClick} onMouseEnter={mouseOver}/>; //React + JSX + два обработчика событий
+function App() {//name of function == name of component. 
+	//In fact, it isn't function. It's component(JSX-element)
+	return (<input placeholder='Some text'//React + JSX 
+	onClick={inputClick} onMouseEnter={mouseOver}/>) //React + JSX + два обработчика событий)
+}
 
 const app = ReactDOMClient.createRoot(document.getElementById("app"));//object, we will place all information
-app.render(elements)// method render let us to place all information
-/*
-метод render возвращает только один родительский тег, т е, если хотим записать два тега, то обернуть в один родительский, н-р div:
-<div>
-<h1></h1>
-<h2></h2>
-</div>
-*/
+app.render(<App />)// method render let us to place all information
 
-//В Reactjs существует технология JSX.
-//Можем сразу писать html код, 
-//и он будет обрабатываться корректно
-//Код с JSX и без: https://itproger.com/course/react-js/3
-
-/*
-ReactDOM.render(React.createElement('input', {
-	placeholder: 'Some text', 
-	value: 'some'
-    onclick: () => console.log("Clicked") //обработчик событий
-}), document.getElementById("app");
-*/
+//Now we will use components.
+//To create new component, you can create new function or class
